7bbbf0a2dfb0aec7162170ce9c73e50d8a7dcd75
Garius6 <gaidarforprivateusage@gmail.com>
2024-05-12 19:04:19 +0300

    Moved main file in cmd directory

--- cmd/kirgu_employee/main.go
+++ cmd/kirgu_employee/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"kirgu.ru/employee/repository"
 	"kirgu.ru/employee/server"
 )
 
 func main() {
 	e := echo.New()
-	repo := NewMapRepository()
+	repo := repository.NewMapRepository()
 	server := server.NewServer(e, repo)
 	server.Start()
 }
